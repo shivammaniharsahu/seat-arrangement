00f65698e525401747c956f991cb12f00d7698c8
shivammaniharsahu <shivam.raipur1234@gmail.com>
2019-09-04 23:25:28 +0530

    first commit

---
+++ 15 train seat arrangement1.cpp
@@ -0,0 +1,190 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i,n,t,k,j,s;
+	cin>>t;
+	for(i=0;i<t;i++)
+	{
+			cin>>n;
+	if(n<=12)
+	{
+		s=13-n;
+		cout<<s;
+		k=abs(s-n);
+		if(k==11||k==1)
+		{
+			cout<<" WS";
+		}
+		
+		else if(k==9||k==3)
+		{
+			cout<<" MS";
+		}
+		else if(k==7||k==5)
+		{
+			cout<<" AS";
+		}
+	}
+	
+	else if(n<=24)
+	{
+		s=37-n;
+		cout<<s;
+		k=abs(s-n);
+		if(k==11||k==1)
+		{
+			cout<<" WS";
+		}
+		
+		else if(k==9||k==3)
+		{
+			cout<<" MS";
+		}
+		else if(k==7||k==5)
+		{
+			cout<<" AS";
+		}
+	}
+	
+	else if(n<=36)
+	{
+		s=61-n;
+		cout<<s;
+		k=abs(s-n);
+		if(k==11||k==1)
+		{
+			cout<<" WS";
+		}
+		
+		else if(k==9||k==3)
+		{
+			cout<<" MS";
+		}
+		else if(k==7||k==5)
+		{
+			cout<<" AS";
+		}
+	}
+	
+	else if(n<=48)
+	{
+		s=85-n;
+		cout<<s;
+		k=abs(s-n);
+		if(k==11||k==1)
+		{
+			cout<<" WS";
+		}
+		
+		else if(k==9||k==3)
+		{
+			cout<<" MS";
+		}
+		else if(k==7||k==5)
+		{
+			cout<<" AS";
+		}
+	}
+	
+	else if(n<=60)
+	{
+		s=109-n;
+		cout<<s;
+		k=abs(s-n);
+		if(k==11||k==1)
+		{
+			cout<<" WS";
+		}
+		
+		else if(k==9||k==3)
+		{
+			cout<<" MS";
+		}
+		else if(k==7||k==5)
+		{
+			cout<<" AS";
+		}
+	}
+	
+	else if(n<=72)
+	{
+		s=133-n;
+		cout<<s;
+		k=abs(s-n);
+		if(k==11||k==1)
+		{
+			cout<<" WS";
+		}
+		
+		else if(k==9||k==3)
+		{
+			cout<<" MS";
+		}
+		else if(k==7||k==5)
+		{
+			cout<<" AS";
+		}
+	}
+	else if(n<=84)
+	{
+		s=157-n;
+		cout<<s;
+		k=abs(s-n);
+		if(k==11||k==1)
+		{
+			cout<<" WS";
+		}
+		
+		else if(k==9||k==3)
+		{
+			cout<<" MS";
+		}
+		else if(k==7||k==5)
+		{
+			cout<<" AS";
+		}
+	}
+	else if(n<=96)
+	{
+		s=181-n;
+		cout<<s;
+		k=abs(s-n);
+		if(k==11||k==1)
+		{
+			cout<<" WS";
+		}
+		
+		else if(k==9||k==3)
+		{
+			cout<<" MS";
+		}
+		else if(k==7||k==5)
+		{
+			cout<<" AS";
+		}
+	}
+	else if(n<=108)
+	{
+		s=205-n;
+		cout<<s;
+		k=abs(s-n);
+		if(k==11||k==1)
+		{
+			cout<<" WS";
+		}
+		
+		else if(k==9||k==3)
+		{
+			cout<<" MS";
+		}
+		else if(k==7||k==5)
+		{
+			cout<<" AS";
+		}
+	}	
+	cout<<endl;
+	}
+	
+	return 0;
+}
